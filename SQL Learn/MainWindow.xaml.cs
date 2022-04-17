9b191544dda3c1d6c7a933ef86b993138d63b639
Fedyaev Andrey <Fedyaev-A-A@mail.ru>
2022-04-17 15:25:47 +0500

    корректировка полей авторизации

--- SQL Learn/MainWindow.xaml.cs
+++ SQL Learn/MainWindow.xaml.cs
@@ -29,10 +29,56 @@ namespace SQL_Learn
         private void Button_Reg_Click(object sender, RoutedEventArgs e)
         {
             string Login, Password1, Password2, Email;
-            Login = TextBoxLogin.Text;
-            Password1 = TextBoxPassword1.Password;
-            Password2 = TextBoxPassword2.Password;
-            Email = TextBoxEmail.Text;
+            Login = TextBoxLogin.Text.Trim();
+            Password1 = TextBoxPassword1.Password.Trim();
+            Password2 = TextBoxPassword2.Password.Trim();
+            Email = TextBoxEmail.Text.Trim().ToLower();
+
+            ///
+            if (Login.Length < 3)
+            {
+                TextBoxLogin.ToolTip = "Необходимо ввести не менее 3 символов";
+                TextBoxLogin.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                TextBoxLogin.ToolTip = "";
+                TextBoxLogin.Background = Brushes.Transparent;
+            }
+            ///
+            if (Password1.Length < 6)
+            {
+                TextBoxPassword1.ToolTip = "Необходимо ввести не менее 6 символов";
+                TextBoxPassword1.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                TextBoxPassword1.ToolTip = "";
+                TextBoxPassword1.Background = Brushes.Transparent;
+            }
+            ///
+            if (Password1 != Password2)
+            {
+                TextBoxPassword2.ToolTip = "Пароли не совпадают";
+                TextBoxPassword2.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                TextBoxPassword2.ToolTip = "";
+                TextBoxPassword2.Background = Brushes.Transparent;
+
+            }
+            ///
+            if (!Email.Contains("@") || !Email.Contains("."))
+            {
+                TextBoxEmail.ToolTip = "Некорректно введен Email";
+                TextBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                TextBoxEmail.ToolTip = "";
+                TextBoxEmail.Background = Brushes.Transparent;
+            }
         }
     }
 }
