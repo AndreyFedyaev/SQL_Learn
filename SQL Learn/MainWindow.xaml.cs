89c618fac4806c73d6e4363e683f2e6b708e2ee7
Fedyaev Andrey <Fedyaev-A-A@mail.ru>
2022-04-17 14:38:52 +0500

    добавление проекта

---
+++ SQL Learn/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQL_Learn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string Login, Password1, Password2, Email;
+            Login = TextBoxLogin.Text;
+            Password1 = TextBoxPassword1.Password;
+            Password2 = TextBoxPassword2.Password;
+            Email = TextBoxEmail.Text;
+        }
+    }
+}
---
+++ SQL Learn/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Learn
+{
+    internal class User
+    {
+        private int ID { get; set; }
+        private string Login, Password, email;
+
+        public User() { }
+
+        public User(string Login, string Password, string email)
+        {
+            this.Login = Login;
+            this.Password = Password;
+            this.email = email;
+        }
+        
+    }
+}
